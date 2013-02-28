d0ba0f59226dcfa23eba97ee291ef02a5ff62c63
Martin Keegan <martin@no.ucant.org>
2013-02-28 12:05:59 +0000

    fix install

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -4,12 +4,13 @@ set -eu
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update > /dev/null
-apt-get --yes install git rabbitmq-server postgresql-9.1
+apt-get --yes install git rabbitmq-server postgresql-9.1 \
+	python-virtualenv python-dev
 
 cd /tmp
 wget http://www.mirrorservice.org/sites/ftp.apache.org/lucene/solr/4.1.0/solr-4.1.0.tgz
 cd ~
-tar xzvf /tmp/solr-4.1.0.tgz
+tar xzf /tmp/solr-4.1.0.tgz
 
 sudo -u postgres createuser -s vagrant
 
