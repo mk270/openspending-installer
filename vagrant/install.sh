f72cf0d593bf28a757888cf3908f78e488cdfe3a
Martin Keegan <martin@no.ucant.org>
2013-03-30 02:58:44 +0000

    put everything in shell functions

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -7,36 +7,64 @@ set -x
 SOLR_URL=http://mk.ucant.org/media/solr-4.1.0-smaller.tgz
 SOLR_TGZ=/tmp/solr-4.1.0-smaller.tgz
 
-export DEBIAN_FRONTEND=noninteractive
-apt-get update > /dev/null
-apt-get --yes install git rabbitmq-server postgresql-9.1 \
-	python-virtualenv python-dev \
-	libxslt1-dev postgresql-server-dev-9.1 \
-	make
+install-packages () {
+	export DEBIAN_FRONTEND=noninteractive
+	apt-get update > /dev/null
+	apt-get --yes install git rabbitmq-server postgresql-9.1 \
+		python-virtualenv python-dev \
+		libxslt1-dev postgresql-server-dev-9.1 \
+		make
+}
 
-$(dirname $0)/fix-pg-auth
+do-fix-pg-auth () {
+	$(dirname $0)/fix-pg-auth
+}
 
-if ! [ "$TRAVIS" -o "$CI" ]; then
-	fix_pghba && /etc/init.d/postgreql restart \
-		|| (cat /etc/postgresql/9.1/main/pg_hba.conf; false)
-fi
+start_postgres () {
+	if ! [ "$TRAVIS" -o "$CI" ]; then
+		do-fix-pg-auth && /etc/init.d/postgreql restart \
+			|| (cat /etc/postgresql/9.1/main/pg_hba.conf; false)
+	fi
+}
 
-(cd /tmp; wget -O $SOLR_TGZ $SOLR_URL)
-(cd ~; tar xzf $SOLR_TGZ)
+install-solr () {
+	(cd /tmp; wget -O $SOLR_TGZ $SOLR_URL)
+	(cd ~; tar xzf $SOLR_TGZ)
+}
 
-sudo -u postgres createuser -s openspending
-sudo -u postgres psql -c "alter user openspending password 'openspending';"
+setup-postgres-users () {
+	sudo -u postgres createuser -s openspending
+	sudo -u postgres psql -c "alter user openspending password 'openspending';"
+}
 
-rm -rf -- openspending-installer
-git clone https://github.com/openspending/openspending-installer
+install-os-installer () {
+	rm -rf -- openspending-installer
+	git clone https://github.com/openspending/openspending-installer
+}
 
-./openspending-installer/install \
-	--db-user openspending \
-	--solr-dir ~/solr-4.1.0 \
-	--no-virtualenv \
-	--no-clone
+run-os-installer () {
+	./openspending-installer/install \
+		--db-user openspending \
+		--solr-dir ~/solr-4.1.0 \
+		--no-virtualenv \
+		--no-clone
+}
 
 # this probably ought to be in a different location
 
-$(dirname $0)/setup-test.ini
-$(dirname $0)/boot-solr &
+setup-test-ini () {
+	$(dirname $0)/setup-test.ini
+}
+
+boot-solr () {
+	$(dirname $0)/boot-solr &
+}
+
+install-packages
+start-postgres
+install-solr
+setup-postgres-users
+install-os-installer
+run-os-installer
+setup-test-ini
+boot-solr
